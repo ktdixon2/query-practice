0896e69bb1c6dcbb83ca007fdec26c22b329231f
Ken Dixon <KDixon@Kens-Macbook-Air.local>
2019-04-10 15:55:28 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+console.log("hello again");
+
+const section = document.querySelector(".article__header");
+section.textContent = "Welcome to Ken's Blog";
+
+const name = document.querySelector(".article__header");
+name.classList.remove("article__header");
+name.classList.add("article__headerImportant");
+
+const footer = document.querySelector(".article__footer");
+footer.classList.add("goldenrod");
+
